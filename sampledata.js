e9f4161714d0dddc8f05cae1418739b89387ee42
Bianca Dennis <bianca.dennis@caa.columbia.edu>
2021-03-21 20:30:18 -0400

    sample data

---
+++ sampledata.js
@@ -0,0 +1,73 @@
+export const userSettings = {
+    lastUpdatedRecurringDate: '3/3/3333'
+}
+//transactions
+export const transations = {
+    results: [
+        {
+            id: 1,
+            name: 'some name',
+            date: '02/12/2121',
+            categoryId: 1,
+            amount: 3.88,
+            month: 1,
+            year: '2021',
+            recurring: false,
+        },{
+            id: 2,
+            name: 'some other name',
+            date: '03/12/2021',
+            categoryId: 2,
+            amount: 8.99,
+            month: 0,
+            year: '2021',
+            recurring: false
+        },
+    ]
+}
+//income
+export const income = {
+    results: [
+        {
+            id: 1,
+            amount: 2000,
+            stable: true
+        }
+    ]
+}
+//categories
+export const categories = {
+    results: [
+        {
+            id: 1,
+            label: 'some category'
+        },{
+            id: 2,
+            label: 'another category'
+        },
+    ]
+}
+//recurring monthly
+export const recurringTransactions = {
+    results: [
+        {
+            id: 1,
+            name: 'some name',
+            date: '02/12/2121',
+            categoryId: 1,
+            amount: 3.88,
+            month: 1,
+            year: '2021',
+            recurring: false,
+        },{
+            id: 2,
+            name: 'some other name',
+            date: '03/12/2021',
+            categoryId: 2,
+            amount: 8.99,
+            month: 0,
+            year: '2021',
+            recurring: false
+        },
+    ]
+}
